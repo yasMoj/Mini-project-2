b3d944598af3d8ae872f4370d260d56cbef297c3
byte-sized-sushi <bytesizedsushi@gmail.com>
2022-09-07 11:11:53 +0200

    La till funktionalitet för att visa items

--- src/main/java/com/gruppprojekt2/kvarteret/ItemController.java
+++ src/main/java/com/gruppprojekt2/kvarteret/ItemController.java
@@ -5,9 +5,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 public class ItemController {
+
+    private static final int PAGE_SIZE = 10;
+
     @Autowired
     ItemRepository itemRepository;
 
@@ -16,5 +22,29 @@ public class ItemController {
         Item item1 = itemRepository.getItem(id);
         model.addAttribute("item", item1);
         return "item";
+    }
+
+    @GetMapping("/items")
+    public String items(Model model, @RequestParam(value="page", required=false, defaultValue="1") int page)
+    {
+        List<Item> items = itemRepository.getPage(page-1, PAGE_SIZE);
+        int pageCount = itemRepository.numberOfPages(PAGE_SIZE);
+        int[] pages = toArray(pageCount);
+
+        model.addAttribute("items", items);
+        model.addAttribute("pages", pages);
+        model.addAttribute("currentPage", page);
+        model.addAttribute("showPrev", page > 1);
+        model.addAttribute("showNext", page < pageCount);
+
+        return "items";
+    }
+
+    private int[] toArray(int num) {
+        int[] result = new int[num];
+        for (int i = 0; i < num; i++) {
+            result[i] = i+1;
+        }
+        return result;
     }
 }
--- src/main/java/com/gruppprojekt2/kvarteret/ItemRepository.java
+++ src/main/java/com/gruppprojekt2/kvarteret/ItemRepository.java
@@ -2,12 +2,39 @@ package com.gruppprojekt2.kvarteret;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
 @Service
 public class ItemRepository {
 
-
 private List<Item> itemsList;
+
+    // Initierar några objekt bara för att testa
+    public ItemRepository()
+    {
+        itemsList = new ArrayList<>();
+
+        for(int i = 1;i<=20;i++)
+        {
+            String productName = "";
+            int randomProduct = ThreadLocalRandom.current().nextInt(0, 6);
+            int randomPrice = ThreadLocalRandom.current().nextInt(20, 5000);
+
+            switch (randomProduct) {
+                case 0 -> productName = "Elvisp";
+                case 1 -> productName = "Såg";
+                case 2 -> productName = "Dammsugare";
+                case 3 -> productName = "Borrmaskin";
+                case 4 -> productName = "Häst";
+                case 5 -> productName = "Cykelvagn";
+            }
+
+            itemsList.add(new Item(i,productName,randomPrice, "Det här är bara en tom beskrivning av annons " + i));
+        }
+    }
+
     public List<Item> getItems() {
         return itemsList;
     }
@@ -20,4 +47,21 @@ private List<Item> itemsList;
         }
         return null;
     }
+
+    public List<Item> items()
+    {
+        return itemsList;
+    }
+
+    public List<Item> getPage(int page, int pageSize) {
+        int from = Math.max(0,page*pageSize);
+        int to = Math.min(itemsList.size(),(page+1)*pageSize);
+
+        return itemsList.subList(from, to);
+    }
+
+    public int numberOfPages(int pageSize) {
+        return (int)Math.ceil((double)itemsList.size() / pageSize);
+    }
+
 }
