b6bf58f1caa10222224ef5db1181ac52eaad3964
RR <rebecca.rivera@awacademy.training>
2022-09-07 10:09:40 +0200

    itemlista

--- src/main/java/com/gruppprojekt2/kvarteret/ItemRepository.java
+++ src/main/java/com/gruppprojekt2/kvarteret/ItemRepository.java
@@ -1,4 +1,23 @@
 package com.gruppprojekt2.kvarteret;
 
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
 public class ItemRepository {
+
+
+private List<Item> itemsList;
+    public List<Item> getItems() {
+        return itemsList;
+    }
+
+    public Item getItem(int id) {
+        for (Item item : itemsList) {
+            if (item.getId() == id) {
+                return item;
+            }
+        }
+        return null;
+    }
 }
