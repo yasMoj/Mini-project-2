b7c997d854dfe254d5ebe7830c83b292efbe641b
UlfHéden <uffeheden@gmail.com>
2022-09-22 11:26:36 +0200

    Log files in user

--- src/main/java/com/gruppprojekt2/kvarteret/UserController.java
+++ src/main/java/com/gruppprojekt2/kvarteret/UserController.java
@@ -10,18 +10,22 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.validation.Valid;
 
 @Controller
 public class UserController {
  //   List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
-
+ Logger logger = LoggerFactory.getLogger(UserController.class);
     @Autowired
     UserSqlRepository repository;
 
+
     @GetMapping("/")
     String start() {
+        logger.info("Logging is running");
         return "startpage";
     }
 
@@ -36,18 +40,22 @@ public class UserController {
     public String showRegistrationForm(Model model) {
         Siteuser user = new Siteuser();
         model.addAttribute("user", user);
+        logger.info("User tries to make account: " + user);
         return "newUser";
     }
 
     @PostMapping("/newUser")
     public String registerUserAccount(@Valid Siteuser user, BindingResult bindingResult, Model model)
     {
-        if(bindingResult.hasErrors())
+        if(bindingResult.hasErrors()) {
+            //logger.error("Logging an USER RESULT ERROR message");
             return "newUser";
+        }
         else{
             model.addAttribute("user",user);
             SecurityConfig.addUser(user.email,user.password);
             repository.save(user);
+            logger.info("User have made new account: " + user.firstName + " " + user.lastName + " " + user.email);
         }
 
         return "newUser";
